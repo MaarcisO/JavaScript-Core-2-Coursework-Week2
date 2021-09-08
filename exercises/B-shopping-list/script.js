4e504e04cbb765465201233377335603e52409cb
Maarcis <markbodonnell@gmail.com>
2021-09-08 23:33:44 +0100

    Completed Exercise C

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,15 @@
+let content = document.getElementById("content");
+
 function shoppingList(arrayOfPeople) {
-  // Write your code here...
+  let ul = document.createElement("UL");
+
+  for(i = 0; i < arrayOfPeople.length; i++){
+    let li = document.createElement("LI");
+    let text = document.createTextNode(arrayOfPeople[i]);
+    content.appendChild(ul);
+    ul.appendChild(li);
+    li.appendChild(text);
+   }
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
