5bd5af62537b535f2d4ff0c02170c18003520b45
Maarcis <markbodonnell@gmail.com>
2021-09-13 14:51:18 +0100

    Exercise D todo list

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,6 +1,24 @@
+let content = document.getElementById("content");
+
+
 function todoList(todos) {
-  // Write your code here...
-}
+  let ul = document.createElement("ul");
+  content.appendChild(ul);
+  for(i = 0; i < todos.length; i++){
+    let li = document.createElement("li");
+    let text = document.createTextNode(todos[i].todo);
+    ul.appendChild(li);
+    li.appendChild(text); 
+    
+    document.querySelector("li").addEventListener("click", function(){
+     document.querySelector("li").style.textDecoration = "line-through"; 
+    });
+       
+  }
+ 
+
+  }
+  
 
 const todos = [
   {todo: "wash the dishes"}, 
--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,7 +1,37 @@
+let content = document.getElementById("content");
+let select = document.createElement("select");
+content.appendChild(select);
+
+
 function listOfColours(colours) {
-  // Write your code here...
+  for(i = 0; i < colours.length; i++){
+    let option = document.createElement("option");
+    let colour = document.createTextNode(colours[i]);
+    select.appendChild(option);
+    option.appendChild(colour);
+
+   
+  }
+  
+ option.forEach(function(i){
+   i.addEventListener("click", function(){
+    pText.style.colour = colours[i]
+   });
+ });
+
+
 }
+
+
+
+let p = document.createElement("p");
+let pText = document.createTextNode("You have selected: " );
+content.appendChild(p);
+p.appendChild(pText);
 
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
 
+
+
 listOfColours(colours);
+
